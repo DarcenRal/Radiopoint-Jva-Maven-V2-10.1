29e587aee9dec8b4a1ff23dcb51cc7958f0138a2
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-05-22 14:29:05 +0300

    add Radiopoint.java and update pom.xml

---
+++ src/main/java/Radiopoint.java
@@ -0,0 +1,101 @@
+public class Radiopoint {
+    private int minVolume = 0;
+    private int maxVolume = 10;
+    private int minChannel = 0;
+    private int maxChannel = 9;
+    private int currentVolume;
+    private int currentChannel;
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinChannel() {
+        return minChannel;
+    }
+
+    public void setMinChannel(int minChannel) {
+        this.minChannel = minChannel;
+    }
+
+    public int getMaxChannel() {
+        return maxChannel;
+    }
+
+    public void setMaxChannel(int maxChannel) {
+        this.maxChannel = maxChannel;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            this.currentVolume = maxVolume;
+            return;
+        }
+        if (currentVolume < minVolume) {
+            this.currentVolume = minVolume;
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
+
+    public void setCurrentChannel(int currentChannel) {
+        if (currentChannel>maxChannel) {
+            this.currentChannel = maxChannel;
+            return;
+        }
+        if (currentChannel<minChannel) {
+            this.currentChannel = minChannel;
+            return;
+        }
+        this.currentChannel = currentChannel;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        currentVolume++;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        currentVolume--;
+    }
+
+    public void increaseChannel() {
+        if (currentChannel == maxChannel) {
+            this.currentChannel = minChannel;
+            return;
+        }
+        currentChannel++;
+    }
+
+    public void decreaseChannel() {
+        if (currentChannel == minChannel) {
+            this.currentChannel = maxChannel;
+            return;
+        }
+        currentChannel--;
+    }
+}
